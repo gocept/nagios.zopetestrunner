5dd8daa3381451e3e847d921ee21b1f0dc8b82bc
Thomas Lotze <164362+tlotze@users.noreply.github.com>
2011-04-26 08:44:48 +0200

    added tests for the terminal and logging output of the nagios check script

--- src/nagios/zopetestrunner/tests/fixture/tests.py
+++ src/nagios/zopetestrunner/tests/fixture/tests.py
@@ -1,10 +1,13 @@
 import unittest
 
 
-class TestCaseA(unittest.TestCase):
+class TestCase(unittest.TestCase):
 
     def test_foo(self):
         pass
 
     def test_bar(self):
         self.fail()
+
+    def test_baz(self):
+        raise RuntimeError
--- src/nagios/zopetestrunner/tests/test_plugin.py
+++ src/nagios/zopetestrunner/tests/test_plugin.py
@@ -2,6 +2,7 @@
 # See also LICENSE.txt
 
 import os.path
+import re
 import subprocess
 import sys
 import unittest
@@ -10,6 +11,12 @@ import unittest
 fixture = os.path.join(os.path.dirname(__file__), 'fixture')
 check_zopetestrunner = os.path.join(fixture, 'check_zopetestrunner.py')
 
+logfile_pattern = re.compile(r'check_testrunner\.[^\.]+\.log')
+
+
+def normalize_logfile(out):
+    return logfile_pattern.sub('check_testrunner.XXX.log', out)
+
 
 class TestPlugin(unittest.TestCase):
 
@@ -17,8 +24,8 @@ class TestPlugin(unittest.TestCase):
         out, _ = subprocess.Popen(
             [sys.executable, check_zopetestrunner, '--', '--path', fixture],
             stdout=subprocess.PIPE).communicate()
-        self.assertTrue('run=2.0' in out)
-        self.assertTrue('errors=0.0' in out)
+        self.assertTrue('run=3.0' in out)
+        self.assertTrue('errors=1.0' in out)
         self.assertTrue('failures=1.0' in out)
 
     def test_options_left_of_dashdash_refer_to_nagios(self):
@@ -51,3 +58,80 @@ class TestPlugin(unittest.TestCase):
         # we don't see the nagios help, no testing is attempted anymore
         self.assertFalse('-t TIMEOUT' in out)
         self.assertFalse('run=' in out)
+
+    def test_nagios_output_ok(self):
+        out, _ = subprocess.Popen(
+            [sys.executable, check_zopetestrunner, '--',
+             '--path', fixture, '-t', 'foo'],
+            stdout=subprocess.PIPE).communicate()
+        self.assertEqual("""\
+ZOPE OK | run=1.0 errors=0.0;;0 failures=0.0;;0
+""", out)
+
+    def test_nagios_output_failure(self):
+        out, _ = subprocess.Popen(
+            [sys.executable, check_zopetestrunner, '--',
+             '--path', fixture, '-t', 'bar'],
+            stdout=subprocess.PIPE).communicate()
+        self.assertEqual("""\
+ZOPE CRITICAL - failures value 1.0 exceeds critical range 0 | run=1.0
+Test runner output logged to /tmp/check_testrunner.XXX.log | errors=0.0;;0
+failures=1.0;;0
+""", normalize_logfile(out))
+
+    def test_nagios_output_error(self):
+        out, _ = subprocess.Popen(
+            [sys.executable, check_zopetestrunner, '--',
+             '--path', fixture, '-t', 'baz'],
+            stdout=subprocess.PIPE).communicate()
+        self.assertEqual("""\
+ZOPE CRITICAL - errors value 1.0 exceeds critical range 0 | run=1.0
+Test runner output logged to /tmp/check_testrunner.XXX.log | errors=1.0;;0
+failures=0.0;;0
+""", normalize_logfile(out))
+
+    def test_failures_are_logged(self):
+        out, _ = subprocess.Popen(
+            [sys.executable, check_zopetestrunner, '--',
+             '--path', fixture, '-t', 'bar'],
+            stdout=subprocess.PIPE).communicate()
+        logfile = out.splitlines()[1].split()[-3]
+        logged = open(logfile).read()
+        self.assertTrue(re.match("""\
+Running zope.testrunner.layer.UnitTests tests:
+  Set up zope.testrunner.layer.UnitTests in .* seconds.
+
+
+Failure in test test_bar \\(tests.TestCase\\)
+Traceback \\(most recent call last\\):
+.*
+    raise self.failureException, msg
+AssertionError
+
+  Ran 1 tests with 1 failures and 0 errors in .* seconds.
+Tearing down left over layers:
+  Tear down zope.testrunner.layer.UnitTests in .* seconds.
+""", logged, re.DOTALL))
+
+    def test_errors_are_logged(self):
+        out, _ = subprocess.Popen(
+            [sys.executable, check_zopetestrunner, '--',
+             '--path', fixture, '-t', 'baz'],
+            stdout=subprocess.PIPE).communicate()
+        logfile = out.splitlines()[1].split()[-3]
+        logged = open(logfile).read()
+        self.assertTrue(re.match("""\
+Running zope.testrunner.layer.UnitTests tests:
+  Set up zope.testrunner.layer.UnitTests in .* seconds.
+
+
+Error in test test_baz \\(tests.TestCase\\)
+Traceback \\(most recent call last\\):
+.*
+    raise RuntimeError
+RuntimeError
+
+  Ran 1 tests with 0 failures and 1 errors in .* seconds.
+Tearing down left over layers:
+  Tear down zope.testrunner.layer.UnitTests in .* seconds.
+""", logged, re.DOTALL))
